c56789acf00aa07d07ccc0c60f54eee81b822121
rCarichino <r.carichino@studenti.uniba.it>
2024-05-14 23:46:29 +0200

    Added new assets, added an Error Dialog widget, modified the Detail Page and the images in the resources directory

--- lib/core/resources/images.dart
+++ lib/core/resources/images.dart
@@ -3,5 +3,6 @@ class Images {
 
   static String icLauncher = "assets/images/ic_launcher.png";
   static String icLogo = "assets/images/ic_logo.png";
-
+  static String posterPlaceholder = "assets/images/placeholder_poster";
+  static String loading = "assets/images/loading.gif";
 }
--- lib/features/movie/presentation/details/detail_page.dart
+++ lib/features/movie/presentation/details/detail_page.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/resources/dimens.dart';
+import '../../../../core/resources/images.dart';
+import '../list/cubits/list_cubit.dart';
 
 class DetailPage extends StatelessWidget {
-   DetailPage({super.key, required this.id});
-String id;
+  const DetailPage({super.key, required this.id});
+
+  final String id;
+
   @override
   Widget build(BuildContext context) {
     int index = int.parse(id);
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Details"),
+      ),
+      body: BlocBuilder<ListCubit, ListState>(
+        builder: (BuildContext context, ListState state) {
+          return Column(
+            children: [
+              Row(
+                children: [
+                  FadeInImage.assetNetwork(
+                      height: 300,
+                      width: 300,
+                      placeholder: Images.loading,
+                      image: state.movie[index].posterPath ??
+                          Images.posterPlaceholder)
+                ],
+              ),
+              Row(
+                children: [
+                  Text("${state.movie[index].title}",
+                      style: TextStyle(
+                          fontSize: Dimens.headlineMedium,
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.w800)),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
