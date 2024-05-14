e917795a48f1bfbab1d4ad589f8115a8522cdabd
rCarichino <r.carichino@studenti.uniba.it>
2024-05-14 22:07:38 +0200

    Modified the search params including a new param

--- lib/features/movie/domain/entities/search_params.dart
+++ lib/features/movie/domain/entities/search_params.dart
@@ -1,23 +1,27 @@
 class SearchParams{
   SearchParams({
       this.query, 
-      this.year,});
+      this.year,
+  this.page});
 
   SearchParams.fromJson(dynamic json) {
     query = json['query'];
     year = json['year'];
+    page = json["page"];
   }
   String? query;
   String? year;
+  int? page;
   SearchParams copyWith({  String? query,
-  String? year,
+  String? year, int? page
 }) => SearchParams(  query: query ?? this.query,
-  year: year ?? this.year,
+  year: year ?? this.year, page: page ?? this.page
 );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['query'] = query;
     map['year'] = year;
+    map["page"] = page;
     return map;
   }
 
