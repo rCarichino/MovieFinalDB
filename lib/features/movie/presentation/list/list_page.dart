807c5bd2327950d61c739832a486fb2539a111bc
marcomastrolonardo <marco24091999@gmail.com>
2024-05-15 00:08:34 +0200

    Modifiche

--- lib/features/movie/presentation/list/list_page.dart
+++ lib/features/movie/presentation/list/list_page.dart
@@ -1,10 +1,83 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:moviedb/core/resources/palette.dart';
+
+import '../../../../core/routes/app_routes.dart';
+import 'cubits/list_cubit.dart';
 
 class ListPage extends StatelessWidget {
   const ListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final formKey = GlobalKey<FormState>();
+
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Palette.redLatte, Palette.blueLatte],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+        ),
+        title: BlocBuilder<ListCubit, ListState>(
+          builder: (context, state) {
+            return Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                      style: const TextStyle(
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w300,
+                        color: Palette.text,
+                      ),
+                      validator: (value) => _validate(value),
+                      onFieldSubmitted: (value) => context
+                          .read<ListCubit>()
+                          .getSearchedMovies(query: value.trim())),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+      body: BlocBuilder<ListCubit, ListState>(
+        builder: (context, state) {
+          if (state.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (state.error.isNotEmpty && !state.isLoading) {
+            return ListView.separated(
+              itemBuilder: (context, index) => GestureDetector(
+                onTap: () => GoRouter.of(context)
+                    .go("/${Routes.details.name}/:${state.movie[index].id}"),
+                child: Column(
+                  children: [
+                    Text("${state.movie[index].originalTitle}"),
+                    Text("${state.movie[index].title}"),
+                  ],
+                ),
+              ),
+              itemCount: state.movie.length,
+              separatorBuilder: (context, index) => const Divider(),
+            );
+          }
+          return const Center(
+            child: Text("Search for a Movie"),
+          );
+        },
+      ),
+    );
+  }
+}
+
+String? _validate(value) {
+  if (value == null) {
+    return "Look for something to search first";
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:moviedb/core/routes/app_routes.dart';
 
+import 'features/movie/presentation/list/cubits/list_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -9,14 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => ListCubit(postPopularMovie: postPopularMovie, postSearchedMovie: postSearchedMovie),
+      child: MaterialApp.router(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          routerConfig: AppRoute.router,
       ),
-      home: const Placeholder()
     );
   }
 }
