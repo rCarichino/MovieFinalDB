66dbf46fab31807ca698c85c82db569d1fbad114
marcomastrolonardo <marco24091999@gmail.com>
2024-05-14 19:54:39 +0200

    Configurazione api

---
+++ lib/core/api/list_api.dart
@@ -0,0 +1,7 @@
+class ListApi{
+  ListApi._();
+  static const kApiHost = 'api.themoviedb.org/3';
+  static const kDiscoverMoviePath = '/discover/movie';
+  static const kSearchMoviePath = '/search/movie';
+  static const kDetailsMoviePath = '/movie/';
+}
