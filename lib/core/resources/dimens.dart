d40a2ac5cf811a7529a658010b185e26fe6a6d6e
rCarichino <r.carichino@studenti.uniba.it>
2024-05-14 20:10:30 +0200

    Added all the resources in the resources directory and created the exception and failure class, also created the abstract use cases

---
+++ lib/core/resources/dimens.dart
@@ -0,0 +1,54 @@
+
+class Dimens {
+  Dimens._();
+
+  static double displayLarge = 96;
+  static double displayMedium = 60;
+  static double displaySmall = 48;
+  static double headlineMedium = 34;
+  static double headlineSmall = 24;
+  static double titleLarge = 20;
+  static double bodyLarge = 16;
+  static double bodyMedium = 14;
+  static double titleMedium = 18;
+  static double titleSmall = 14;
+  static double labelLarge = 16;
+  static double bodySmall = 12;
+  static double labelSmall = 10;
+
+  static double zero = 0;
+  static double space2 = 2;
+  static double space3 = 3;
+  static double space4 = 4;
+  static double space6 = 6;
+  static double space8 = 8;
+  static double space12 = 12;
+  static double space16 = 16;
+  static double space24 = 24;
+  static double space30 = 30;
+  static double space36 = 36;
+  static double space40 = 40;
+  static double space46 = 46;
+  static double space50 = 50;
+
+  static double selectedIndicatorW = 43;
+  static double selectedIndicatorSmallW = 28;
+  static double heightAppbarHome = 65;
+  static double tab = 38;
+  static double menu = 72;
+  static double menuContainer = 250;
+  static double carousel = 167;
+  static double newsHeight = 185;
+
+  static double header = 200;
+  static double minLabel = 116;
+  static double bottomBar = 80;
+  static double profilePicture = 110;
+  static double birthdayCalendar = 120;
+
+  static double buttonH = 40;
+  static double imageW = 110;
+
+  static const double cornerRadius = 15;
+  static const double cornerRadiusBottomSheet = 30;
+}
---
+++ lib/core/resources/images.dart
@@ -0,0 +1,7 @@
+class Images {
+  Images._();
+
+  static String icLauncher = "assets/images/ic_launcher.png";
+  static String icLogo = "assets/images/ic_logo.png";
+
+}
