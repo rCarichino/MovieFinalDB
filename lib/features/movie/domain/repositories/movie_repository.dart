2a5bfdb0e2cbda619be540942a42d8cdddbeba16
rCarichino <r.carichino@studenti.uniba.it>
2024-05-14 22:24:15 +0200

    Added the usecases and the movie repository of the domain layer

---
+++ lib/features/movie/domain/repositories/movie_repository.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:moviedb/core/error/failure.dart';
+
+import '../entities/movie.dart';
+import '../entities/search_params.dart';
+
+abstract class MovieRepository{
+
+  Future<Either<Failure, List<Movie>>> fetchListMovies(SearchParams SearchParams);
+  Future<Either<Failure, List<Movie>>> fetchSearchedMovies(SearchParams searchParams);
+
+}
---
+++ lib/features/movie/domain/usecases/post_popular_movie.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:moviedb/core/error/failure.dart';
+import 'package:moviedb/core/usecase/usecase.dart';
+import 'package:moviedb/features/movie/domain/entities/movie.dart';
+import 'package:moviedb/features/movie/domain/entities/search_params.dart';
+import 'package:moviedb/features/movie/domain/repositories/movie_repository.dart';
+
+class PostSearchedMovie extends UseCase< List<Movie>, SearchParams>{
+
+  final MovieRepository movieRepository;
+  PostSearchedMovie(this.movieRepository);
+
+  @override
+  Future<Either<Failure, List<Movie>>> call(SearchParams params)  => movieRepository.fetchListMovies(params);
+
+
+}
