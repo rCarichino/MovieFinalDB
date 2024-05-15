e6c4f6f428a15f5545ff13d4c9b630f84846a858
marcomastrolonardo <marco24091999@gmail.com>
2024-05-15 13:58:12 +0200

    Progress list_page

--- lib/features/movie/presentation/list/list_page.dart
+++ lib/features/movie/presentation/list/list_page.dart
@@ -24,45 +24,120 @@ class ListPage extends StatelessWidget {
     }
 
     return SafeArea(
-      child: Scaffold(
-          appBar: AppBar(
-              flexibleSpace: Container(
-                decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                  colors: [Palette.redLatte, Palette.blueLatte],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                )),
-              ),
-              title: Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide.none),
-                          filled: true,
-                          fillColor: Palette.textDark,
-                          hintText: "cerca un film",
-                        ),
-                        style: const TextStyle(
-                          fontFamily: "Poppins",
-                          fontWeight: FontWeight.w300,
-                          color: Palette.text,
-                        ),
-                        validator: (value) => _validate(value),
-                        onFieldSubmitted: (value) => context
-                            .read<ListCubit>()
-                            .getSearchedMovies(query: value.trim())),
-                  ],
+      child: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            appBar: AppBar(
+                flexibleSpace: Container(
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Palette.redLatte, Palette.blueLatte],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      )),
                 ),
-              ),
-              actions: [
-                IconButton(onPressed: () {}, icon: const Icon(Icons.search))
-              ]),
-          body: BlocBuilder<ListCubit, ListState>(builder: (context, state) {
+                title: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none),
+                            filled: true,
+                            fillColor: Palette.textDark,
+                            hintText: "cerca un film",
+                          ),
+                          style: const TextStyle(
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.w300,
+                            color: Palette.text,
+                          ),
+                          validator: (value) => _validate(value),
+                          onFieldSubmitted: (value) =>
+                              context
+                                  .read<ListCubit>()
+                                  .getSearchedMovies(query: value.trim())),
+                    ],
+                  ),
+                ),
+                actions: [
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+                ]),
+            body: Column(
+              children: [
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      BlocBuilder<ListCubit, ListState>(builder: (context, state) {
+                        if (state.isLoading) {
+                          return Center(
+                            child: Image.asset(
+                              Images.loading,
+                              height: 125.0,
+                              width: 125.0,
+                            ),
+                          );
+                        }
+                        if (state.movie.isNotEmpty && !state.isLoading) {
+                          return Center(
+                            heightFactor: 1,
+                            child: Container(
+                              color: Palette.backgroundDark,
+                              margin: const EdgeInsets.only(top: 10),
+                              padding:
+                              const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                              width: MediaQuery.of(context).size.width * 0.95,
+                              height: MediaQuery.of(context).size.height * 0.82,
+                              child: GridView.builder(
+                                gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  mainAxisSpacing: 8,
+                                ),
+                                itemBuilder: (context, index) => GestureDetector(
+                                  onTap: () {
+                                    final String detailsRoute =
+                                        "/${Routes.details.name}/${index.toString()}";
+                                    context.go(detailsRoute);
+                                  },
+                                  child: Container(
+                                    padding: const EdgeInsets.all(5),
+                                    color: Palette.cardDark,
+                                    child: CircleImage(
+                                        url: "https://image.tmdb.org/t/p/original${state.movie[index].posterPath}"),
+                                  ),
+                                ),
+                                itemCount: state.movie.length,
+                              ),
+                            ),
+                          );
+                        }
+                        return Center(
+                            child: Image.asset(
+                              Images.loading,
+                              height: 125.0,
+                              width: 125.0,
+                            ));
+                      })
+                    ],
+                  ),
+                ),
+                TabBar(tabs: [
+                  Tab(text: 'Tab1'),
+                  Tab(text: 'Tab2'),
+                  Tab(text: 'Tab3'),
+                ])
+              ],
+            ),
+      )),
+    );
+  }
+}
+
+/*
+BlocBuilder<ListCubit, ListState>(builder: (context, state) {
             if (state.isLoading) {
               return Center(
                 child: Image.asset(
@@ -80,8 +155,8 @@ class ListPage extends StatelessWidget {
                   margin: const EdgeInsets.only(top: 10),
                   padding:
                       const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  height: MediaQuery.of(context).size.height * 0.8,
+                  width: MediaQuery.of(context).size.width * 0.95,
+                  height: MediaQuery.of(context).size.height * 0.82,
                   child: GridView.builder(
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
@@ -112,7 +187,5 @@ class ListPage extends StatelessWidget {
               height: 125.0,
               width: 125.0,
             ));
-          })),
-    );
-  }
-}
+          })
+ */
