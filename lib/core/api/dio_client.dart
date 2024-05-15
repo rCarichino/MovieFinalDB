325da837ed0a0e28d631f30868e42f02bdade172
marcomastrolonardo <marco24091999@gmail.com>
2024-05-15 08:48:43 +0200

    Fix dio_client

--- lib/core/api/dio_client.dart
+++ lib/core/api/dio_client.dart
@@ -1,21 +1,27 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:moviedb/core/error/failure.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 typedef ResponseConverter<T> = T Function(dynamic response);
 
-class DioClient{
+class DioClient {
+  final dio = Dio(BaseOptions(
+    baseUrl: dotenv.env["BASE_URL"] ?? '',
+  ));
 
-  final dio = Dio();
-
-  Future<Either<Failure, T>> getRequest<T>(String url, {required Map<String, dynamic>? queryParameters, required ResponseConverter<T> converter}) async {
-    try{
-      final response = await dio.get(url, queryParameters: queryParameters);
-      if(response.statusCode != 200){
-        throw DioException(requestOptions: response.requestOptions, response: response);
+  Future<Either<Failure, T>> getRequest<T>(String url,
+      {required Map<String, dynamic>? queryParameters,
+      required ResponseConverter<T> converter}) async {
+    try {
+      final response = await dio.get(url,
+          queryParameters: queryParameters);
+      if (response.statusCode != 200) {
+        throw DioException(
+            requestOptions: response.requestOptions, response: response);
       }
       return Right(converter(response.data));
-    }on DioException catch (error){
+    } on DioException catch (error) {
       return Left(ServerFailure(error.message));
     }
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:moviedb/core/routes/app_routes.dart';
 import 'dependencies_injection.dart';
 import 'features/movie/presentation/list/cubits/list_cubit.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await serviceLocator();
   runApp(const MyApp());
 }
