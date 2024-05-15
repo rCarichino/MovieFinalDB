a6a99abf89a31a9133be3789bff90646a053aaba
rCarichino <r.carichino@studenti.uniba.it>
2024-05-15 15:08:28 +0200

    Added the cubits of login and the login page

---
+++ lib/features/auth/presentation/registration/registration_page.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:moviedb/features/auth/presentation/login/cubits/login_cubit.dart';
+import 'package:moviedb/features/auth/presentation/registration/cubits/registration_cubit.dart';
+import 'package:oktoast/oktoast.dart';
+
+import '../../../../core/resources/dimens.dart';
+import '../../../../core/resources/images.dart';
+import '../../../../core/routes/app_routes.dart';
+import '../../../../core/widgets/button.dart';
+import '../../../../core/widgets/button_text.dart';
+import '../../../../core/widgets/spacer_v.dart';
+import '../../../../core/widgets/text_f.dart';
+import '../../../../utils/validator.dart';
+
+class RegistrationPage extends StatefulWidget {
+  const RegistrationPage({super.key});
+
+  @override
+  State<RegistrationPage> createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  /// Controller
+  final _conEmail = TextEditingController();
+  final _conPassword = TextEditingController();
+
+  /// Focus Node
+  final _fnEmail = FocusNode();
+  final _fnPassword = FocusNode();
+
+  /// Global key
+  final _keyForm = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocConsumer<RegistrationCubit, RegistrationState>(listener: (context, state) {
+        if (state.error.isNotEmpty) {
+          showToast(state.error);
+        }
+      }, builder: (context, state) {
+        return Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(Dimens.space24),
+              child: AutofillGroup(
+                child: Form(
+                  key: _keyForm,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Theme.of(context).hintColor,
+                        radius: Dimens.profilePicture + Dimens.space4,
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage(Images.icLauncher),
+                          radius: Dimens.profilePicture,
+                        ),
+                      ),
+                      const SpacerV(),
+                      TextF(
+                        autofillHints: const [AutofillHints.email],
+                        key: const Key("email"),
+                        curFocusNode: _fnEmail,
+                        nextFocusNode: _fnPassword,
+                        textInputAction: TextInputAction.next,
+                        controller: _conEmail,
+                        keyboardType: TextInputType.emailAddress,
+                        prefixIcon: Icon(
+                          Icons.alternate_email,
+                          color: Theme.of(context).textTheme.bodyLarge?.color,
+                        ),
+                        hintText: "example@example.com",
+                        hint: "email",
+                        validator: (String? value) => validateEmail(value),
+                      ),
+                      BlocBuilder<RegistrationCubit, RegistrationState>(
+                        builder: (_, state) {
+                          return TextF(
+                            autofillHints: const [AutofillHints.password],
+                            key: const Key("password"),
+                            curFocusNode: _fnPassword,
+                            textInputAction: TextInputAction.done,
+                            controller: _conPassword,
+                            keyboardType: TextInputType.text,
+                            prefixIcon: Icon(
+                              Icons.lock_outline,
+                              color:
+                              Theme.of(context).textTheme.bodyLarge?.color,
+                            ),
+                            obscureText: state.showPassword,
+                            hintText: '••••••••••••',
+                            maxLine: 1,
+                            hint: "password",
+                            suffixIcon: IconButton(
+                              icon: const Icon(Icons.remove_red_eye_outlined),
+                              onPressed: () => context
+                                  .read<RegistrationCubit>()
+                                  .toggleShowPassword(),
+                            ),
+                            validator: (String? value) =>
+                                isValidPassword(value),
+                          );
+                        },
+                      ),
+                      SpacerV(value: Dimens.space24),
+                      Button(
+                        title: "Register",
+                        onPressed: () {
+                          if (_keyForm.currentState?.validate() ?? false) {
+                            context.read<RegistrationCubit>().registration(
+                              email: _conEmail.text,
+                              password: _conPassword.text,
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
