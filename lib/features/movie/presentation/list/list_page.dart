d3495db56975ceb646c68a41f2e53c9142189c1b
marcomastrolonardo <marco24091999@gmail.com>
2024-05-15 12:54:54 +0200

    Basic list_page setup

--- lib/features/movie/presentation/list/list_page.dart
+++ lib/features/movie/presentation/list/list_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:moviedb/core/resources/palette.dart';
+import 'package:moviedb/core/widgets/circle_image.dart';
 
 import '../../../../core/resources/images.dart';
 import '../../../../core/routes/app_routes.dart';
@@ -15,75 +16,89 @@ class ListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final formKey = GlobalKey<FormState>();
 
-    return Scaffold(
-        appBar: AppBar(
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                colors: [Palette.redLatte, Palette.blueLatte],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              )),
-            ),
-            title: Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                      style: const TextStyle(
-                        fontFamily: "Poppins",
-                        fontWeight: FontWeight.w300,
-                        color: Palette.text,
-                      ),
-                      validator: (value) => _validate(value),
-                      onFieldSubmitted: (value) => context
-                          .read<ListCubit>()
-                          .getSearchedMovies(query: value.trim())),
-                ],
+    String? _validate(value) {
+      if (value == null) {
+        return "Look for something to search first";
+      }
+      return null;
+    }
+
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                  colors: [Palette.redLatte, Palette.blueLatte],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                )),
               ),
-            )),
-        body: BlocBuilder<ListCubit, ListState>(builder: (context, state) {
-          if (state.isLoading) {
-            return Center(
-              child: Image.asset(
-                Images.loading,
-                height: 125.0,
-                width: 125.0,
-              ),
-            );
-          }
-          if (state.movie.isNotEmpty && !state.isLoading) {
-            return ListView.separated(
-              itemBuilder: (context, index) => GestureDetector(
-                onTap: () {
-                  final String detailsRoute =
-                      "/${Routes.details.name}/${index.toString()}";
-                  context.go(detailsRoute);
-                },
+              title: Form(
+                key: formKey,
                 child: Column(
                   children: [
-                    Text("${state.movie[index].originalTitle}"),
-                    Text("${state.movie[index].title}"),
+                    TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none),
+                          filled: true,
+                          fillColor: Palette.textDark,
+                          hintText: "cerca un film",
+                        ),
+                        style: const TextStyle(
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.w300,
+                          color: Palette.text,
+                        ),
+                        validator: (value) => _validate(value),
+                        onFieldSubmitted: (value) => context
+                            .read<ListCubit>()
+                            .getSearchedMovies(query: value.trim())),
                   ],
                 ),
               ),
-              itemCount: state.movie.length,
-              separatorBuilder: (context, index) => const Divider(),
-            );
-          }
-          return Center(
-              child: Image.asset(
-            Images.loading,
-            height: 125.0,
-            width: 125.0,
-          ));
-        }));
+              actions: [
+                IconButton(onPressed: (){}, icon: Icon(Icons.search))
+              ]),
+          body: BlocBuilder<ListCubit, ListState>(builder: (context, state) {
+            if (state.isLoading) {
+              return Center(
+                child: Image.asset(
+                  Images.loading,
+                  height: 125.0,
+                  width: 125.0,
+                ),
+              );
+            }
+            if (state.movie.isNotEmpty && !state.isLoading) {
+              return ListView.separated(
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: () {
+                    final String detailsRoute =
+                        "/${Routes.details.name}/${index.toString()}";
+                    context.go(detailsRoute);
+                  },
+                  child: Column(
+                    children: [
+                      Text("${state.movie[index].originalTitle}"),
+                      Text("${state.movie[index].title}"),
+                      CircleImage(url: "https://image.tmdb.org/t/p/original${state.movie[index].posterPath}")
+                    ],
+                  ),
+                ),
+                itemCount: state.movie.length,
+                separatorBuilder: (context, index) => const Divider(),
+              );
+            }
+            return Center(
+                child: Image.asset(
+              Images.loading,
+              height: 125.0,
+              width: 125.0,
+            ));
+          })),
+    );
   }
-}
-
-String? _validate(value) {
-  if (value == null) {
-    return "Look for something to search first";
-  }
-  return null;
 }
