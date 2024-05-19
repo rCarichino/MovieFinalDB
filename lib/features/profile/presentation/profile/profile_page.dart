bf45e4f80e73c43492bc3c6fb6f4a2a13a7595dd
marcomastrolonardo <marco24091999@gmail.com>
2024-05-19 23:56:19 +0200

    adjustment to profile_page

--- lib/features/profile/presentation/profile/profile_page.dart
+++ lib/features/profile/presentation/profile/profile_page.dart
@@ -6,6 +6,7 @@ import 'package:moviedb/core/widgets/button.dart';
 import 'package:moviedb/features/profile/presentation/profile/cubits/profile_cubit.dart';
 import 'package:moviedb/features/profile/presentation/profile/widgets/container_profile.dart';
 import 'package:moviedb/features/profile/presentation/profile/widgets/delete_user_dialog_profile.dart';
+import 'package:moviedb/features/profile/presentation/profile/widgets/delete_user_section.dart';
 import 'package:moviedb/features/profile/presentation/profile/widgets/send_email_verification_form.dart';
 import 'package:moviedb/features/profile/presentation/profile/widgets/edit_user_profile_form.dart';
 
@@ -47,48 +48,23 @@ class ProfilePage extends StatelessWidget {
           ],
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              ContainerProfile(
-                dimens: MediaQuery.of(context).size.height * 0.40,
-                palette: Palette.cardDark,
-                child: const EditUserProfileForm(),
-              ),
-              ContainerProfile(
-                dimens: MediaQuery.of(context).size.height * 0.25,
-                palette: Palette.background,
-                child: const SendEmailVerificationForm(),
-              ),
-              ContainerProfile(
-                dimens: MediaQuery.of(context).size.height * 0.181,
-                palette: Palette.card,
-                child: Column(
-                  children: [
-                    Container(
-                        padding:
-                            const EdgeInsets.only(right: 25, left: 10, top: 10),
-                        height: 120,
-                        child: SingleChildScrollView(
-                            child: Text(
-                                style: Theme.of(context).textTheme.labelMedium,
-                                "Are you sure you want to delete your account? This action is permanent and cannot be undone."
-                                "\nDeleting your account will result in the following:"
-                                "\nAll your data, including personal information, preferences, and saved content, will be permanently erased."
-                                "\nYou will lose access to all services and features associated with your account."
-                                "\nAny ongoing subscriptions or memberships will be canceled, and any remaining balance or credits will be forfeited."))),
-                    Button(
-                      width: Dimens.buttonH,
-                      title: 'Delete Account',
-                      color: Palette.redLatte,
-                      onPressed: () {
-                        deleteUserDialog(context);
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+          child: Column(children: [
+            ContainerProfile(
+              dimens: MediaQuery.of(context).size.height * 0.40,
+              palette: Palette.cardDark,
+              child: const EditUserProfileForm(),
+            ),
+            ContainerProfile(
+              dimens: MediaQuery.of(context).size.height * 0.25,
+              palette: Palette.background,
+              child: const SendEmailVerificationForm(),
+            ),
+            ContainerProfile(
+              dimens: MediaQuery.of(context).size.height * 0.181,
+              palette: Palette.card,
+              child: DeleteUserSection(),
+            ),
+          ]),
         ),
         bottomNavigationBar: bottomNavigationBar(context));
   }
